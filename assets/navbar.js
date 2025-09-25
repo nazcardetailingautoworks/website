83ec4b39c3598f00aad59ba70a0a17ee95995602
nazcardetailingautoworks <nazcardetailingautoworks@gmail.com>
2025-09-25 11:33:21 +0700

    Delete assets/navbar.js
    25-9-2023
    Merapihkan Media Query

--- assets/navbar.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const toggle = document.querySelector('.navbar-toggle');
-  const menu = document.querySelector('.navbar ul');
-  toggle.addEventListener('click', function() {
-    menu.classList.toggle('active');
-  });
-});
